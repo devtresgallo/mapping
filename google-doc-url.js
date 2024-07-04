af85fac50d94b3e0dcbf18410cf6d17389dcd55b
Daniel Tresgallo <24434932+devtresgallo@users.noreply.github.com>
2024-07-04 11:58:17 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wjvNwbhxUthi9HKcMn25M3ke24FXVMO8Wsg1ElH1a1g/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyCpraDUFDrcXwL_jzqr5I8oO5h2E5hvJ48';
